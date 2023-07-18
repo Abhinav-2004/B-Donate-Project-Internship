6b353dd0fd93ddf09c5dc2ec9ae5d7a4dcd0d7d9
Abhinav Singh <kittusinghranchi@gmail.com>
2023-07-18 14:55:08 +0530

    commit

--- components/pages/Donation_Form/access.js
+++
@@ -1,6 +0,0 @@
-const token = JSON.parse(localStorage.getItem("token"));
-
-if (token === undefined || token === null) {
-  alert("Please Login First To Donate !");
-  window.location.replace("../signin/index.html");
-}
--- components/pages/Eligibility/index.js
+++
@@ -1,5 +0,0 @@
-const red = JSON.stringify(Date.now());
-
-document.getElementById("donate").addEventListener("click", () => {
-  localStorage.setItem("redirect", red);
-});
